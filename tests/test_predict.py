e21e7a1557cc37b379ba98227064d402fdde268e
unknown <rajeev.sharma148@gmail.com>
2024-07-09 23:03:48 +0530

    parent directory of 'src' into sys.path

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,3 +1,9 @@
+import src
+import os
+
+# Insert the parent directory of 'src' into sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 from src.predict import app
 import unittest
 import json

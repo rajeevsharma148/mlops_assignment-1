2c92253198eb8f9bff7e10eddd95ecd62d4ac6ab
unknown <rajeev.sharma148@gmail.com>
2024-08-04 10:49:48 +0530

    test case update

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Insert the parent directory of 'src' into sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from src.predict import app
 import unittest
